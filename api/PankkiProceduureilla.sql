6602ecbcb8a66ae0cb80924fd79c5ca5891443f5
Niibix <t0nani00@students.oamk.fi>
2021-11-17 10:06:49 +0200

    apia päivitelty

--- api/PankkiProceduureilla.sql
+++ api/PankkiProceduureilla.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `pankki_proseduureilla` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `pankkiproseduureilla`;
+CREATE DATABASE  IF NOT EXISTS `pankki` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `pankki`;
 -- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: pankki
