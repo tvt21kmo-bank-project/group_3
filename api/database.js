52dd48ccee29d93c3222e668dd1c517282773819
Jaakko Malmi <j.j.malmi@gmail.com>
2021-11-17 11:40:53 +0200

    database connection siirretty tapahtumaan .env kautta

--- api/database.js
+++ api/database.js
@@ -1,8 +1,14 @@
-const mysql = require('mysql');
+/*const mysql = require('mysql');
 const connection = mysql.createPool({
   host: 'localhost',
   user: 'pankki_admin',
   password: 'bosspankki',
   database: 'pankki'
 });
+module.exports = connection;*/
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const connection = mysql.createConnection(process.env.MYSQL);
 module.exports = connection;
