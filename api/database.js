82eb1eb89141cf6556c0a8d43deaa69f82a66952
Niibix <t0nani00@students.oamk.fi>
2021-11-19 12:32:00 +0200

    Vaihdettu oikea database.js

---
+++ api/database.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+const connection = mysql.createPool({
+  host: 'localhost',
+  user: 'pankki_admin',
+  password: 'bosspankki',
+  database: 'pankki'
+});
+module.exports = connection;
+
+/*const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const connection = mysql.createConnection(process.env.MYSQL);
+module.exports = connection;*/
