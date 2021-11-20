0c9861609b528c7f05eefff2d8c94a86a679b4d9
jmetsaniemi <91074405+jmetsaniemi@users.noreply.github.com>
2021-11-20 11:42:05 +0200

    Update database.js

--- api/database.js
+++ api/database.js
@@ -1,10 +1,8 @@
 const mysql = require('mysql');
 const connection = mysql.createPool({
   host: 'localhost',
-  //user: 'pankki_admin',
-  //password: 'bosspankki',
-  user: 'root',
-  password: 'Lenghtnotio1',
+  user: 'pankki_admin',
+  password: 'bosspankki',
   database: 'pankki'
 });
 module.exports = connection;
